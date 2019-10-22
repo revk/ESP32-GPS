1317514ad1044ba78d1357aed63b6cb9f0f376b2
Adrian Kennard <a@k.gg>
2019-10-22 10:55:48 +0100

    Basic data captured and displayed

--- main/GPS.c
+++ main/GPS.c
@@ -19,13 +19,12 @@ static const char TAG[] = "GPS";
 	u8(gpstx,32)	\
 	u8(gpsfix,25)	\
 	u8(gpsen,26)	\
-	b(oledflip)	\
-	b(f)	\
+	b(oledflip,Y)	\
 
 #define u32(n,d)	uint32_t n;
 #define s8(n,d)	int8_t n;
 #define u8(n,d)	int8_t n;
-#define b(n) uint8_t n;
+#define b(n,d) uint8_t n;
 #define s(n,d) char * n;
 settings
 #undef u32
@@ -44,11 +43,121 @@ app_command (const char *tag, unsigned int len, const unsigned char *value)
    return NULL;
 }
 
+double speed = 0;
+double bearing = 0;
+double lat = 0;
+double lon = 0;
+double alt = 0;
+double gsep = 0;
+double hdop = 0;
+int sats = 0;
+
+static void
+nmea (char *s)
+{
+   if (!s || *s != '$' || s[1] != 'G' || s[2] != 'P')
+      return;
+   char *f[50];
+   int n = 0;
+   s++;
+   while (*s && n < sizeof (f) / sizeof (*f))
+   {
+      f[n++] = s;
+      while (*s && *s != ',')
+         s++;
+      if (!*s || *s != ',')
+         break;
+      *s++ = 0;
+   }
+   if (!n)
+      return;
+   if (!strncmp (f[0], "GPGGA", 5) && n >= 14)
+   {                            // Fix
+      if (strlen (f[2]) > 2)
+         lat = ((f[2][0] - '0') * 10 + f[2][1] - '0' + strtod (f[2] + 2, NULL) / 60) * (f[3][0] == 'N' ? 1 : -1);
+      if (strlen (f[4]) > 3)
+         lon = ((f[4][0] - '0') * 100 + (f[4][1] - '0') * 10 + f[4][2] - '0' + strtod (f[4] + 3, NULL) / 60) * (f[5][0] == 'E' ? 1 : -1);
+      sats = atoi (f[7]);
+      hdop = strtod (f[8], NULL);
+      alt = strtod (f[9], NULL);
+      gsep = strtod (f[10], NULL);
+      return;
+   }
+   if (!strncmp (f[0], "GPRMC", 5) && n >= 12)
+   {
+      if (strlen (f[1]) >= 6 && strlen (f[9]) >= 6)
+      {
+         struct tm t = { };
+         t.tm_hour = (f[1][0] - '0') * 10 + f[1][1] - '0';
+         t.tm_min = (f[1][2] - '0') * 10 + f[1][3] - '0';
+         t.tm_sec = (f[1][4] - '0') * 10 + f[1][5] - '0';
+         t.tm_year = 100 + (f[9][4] - '0') * 10 + f[9][5] - '0';
+         t.tm_mon = (f[9][2] - '0') * 10 + f[9][3] - '0' - 1;
+         t.tm_mday = (f[9][0] - '0') * 10 + f[9][1] - '0';
+         struct timeval v = { };
+         if (f[1][6] == '.')
+            v.tv_usec = atoi (f[1] + 7) * 1000;
+         v.tv_sec = mktime (&t);
+         settimeofday (&v, NULL);
+	 revk_info(TAG,"%d %d %d %d %d %d",t.tm_year,t.tm_mon,t.tm_mday,t.tm_hour,t.tm_min,t.tm_sec);
+      }
+      return;
+   }
+   if (!strncmp (f[0], "GPRMC", 5) && n >= 10)
+   {
+      speed = strtod (f[7], NULL);
+      return;
+   }
+}
+
+static void
+display_task (void *p)
+{
+   p = p;
+   while (1)
+   {
+      sleep (1);
+      if (sats)
+      {
+         oled_lock ();
+         char temp[100];
+         int y = CONFIG_OLED_HEIGHT;
+         {
+            time_t now = time (0);
+            struct tm nowt;
+            localtime_r (&now, &nowt);
+            if (nowt.tm_year > 100)
+            {
+               y -= 10;
+               strftime (temp, sizeof (temp), "%F\004%T %Z", &nowt);
+               oled_text (1, 0, y, temp);
+            }
+         }
+         y -= 10;
+         sprintf (temp, "Sats: %d   ", sats);
+         oled_text (-1, 0, y, temp);
+         y -= 10;
+         sprintf (temp, "Lat: %lf   ", lat);
+         oled_text (-1, 0, y, temp);
+         y -= 10;
+         sprintf (temp, "Lon: %lf   ", lon);
+         oled_text (-1, 0, y, temp);
+         y -= 10;
+         sprintf (temp, "Alt: %6.2lfm   ", alt);
+         oled_text (-1, 0, y, temp);
+         y -= 10;
+         sprintf (temp, "Speed: %6.2lfkm/h   ", speed);
+         oled_text (-1, 0, y, temp);
+         oled_unlock ();
+      }
+   }
+}
+
 void
 app_main ()
 {
    revk_init (&app_command);
-#define b(n) revk_register(#n,0,sizeof(n),&n,NULL,SETTING_BOOLEAN);
+#define b(n,d) revk_register(#n,0,sizeof(n),&n,#d,SETTING_BOOLEAN);
 #define u32(n,d) revk_register(#n,0,sizeof(n),&n,#d,0);
 #define s8(n,d) revk_register(#n,0,sizeof(n),&n,#d,SETTING_SIGNED);
 #define u8(n,d) revk_register(#n,0,sizeof(n),&n,#d,0);
@@ -74,7 +183,7 @@ app_main ()
       revk_error (TAG, "UART param fail %s", esp_err_to_name (err));
    else if ((err = uart_set_pin (gpsuart, gpstx, gpsrx, -1, -1)))
       revk_error (TAG, "UART pin fail %s", esp_err_to_name (err));
-   else if ((err = uart_driver_install (gpsuart, 1000, 0, 0, NULL, 0)))
+   else if ((err = uart_driver_install (gpsuart, 256, 0, 0, NULL, 0)))
       revk_error (TAG, "UART install fail %s", esp_err_to_name (err));
    else
       revk_info (TAG, "PN532 UART %d Tx %d Rx %d", gpsuart, gpstx, gpsrx);
@@ -83,14 +192,53 @@ app_main ()
       gpio_set_level (gpsen, 1);
       gpio_set_direction (gpsen, GPIO_MODE_OUTPUT);
    }
+   revk_task ("Display", display_task, NULL);
    // Main task...
+   uint8_t buf[1000],
+    *p = buf;
    while (1)
    {
-      uint8_t buf[1000];
-      int l = uart_read_bytes (gpsuart, buf, 1000, 10);
+      // Get line(s), the timeout should mean we see one or more whole lines typically
+      int l = uart_read_bytes (gpsuart, p, buf + sizeof (buf) - p, 10);
       if (l < 0)
          sleep (1);
-      else if (l > 0)
-         revk_info ("Rx", "%d [%.*s]", l, l, buf);
+      if (l <= 0)
+         continue;
+      uint8_t *e = p + l;
+      p = buf;
+      while (p < e)
+      {
+         uint8_t *l = p;
+         while (l < e && *l >= ' ')
+            l++;
+         if (l == e)
+            break;
+         if (*p == '$' && (l - p) >= 4 && l[-3] == '*' && isxdigit (l[-2]) && isxdigit (l[-1]))
+         {
+            // Checksum
+            uint8_t c = 0,
+               *x;
+            for (x = p + 1; x < l - 3; x++)
+               c ^= *x;
+            if (((c >> 4) > 9 ? 7 : 0) + (c >> 4) + '0' != l[-2] || ((c & 0xF) > 9 ? 7 : 0) + (c & 0xF) + '0' != l[-1])
+               revk_error (TAG, "[%.*s] (%02X)", l - p, p, c);
+            else
+            {                   // Process line
+               l[-3] = 0;
+               nmea ((char *) p);
+            }
+         } else
+            revk_error (TAG, "[%.*s]", l - p, p);
+         while (l < e && *l < ' ')
+            l++;
+         p = l;
+      }
+      if (p < e && (e - p) < sizeof (buf))
+      {                         // Partial line
+         memmove (buf, p, e - p);
+         p = buf + (e - p);
+         continue;
+      }
+      p = buf;                  // Start from scratch
    }
 }
