7523cd870e0228e6bd5c0e0f9d48ac2500dd7c51
Adrian Kennard <a@k.gg>
2019-10-30 15:36:49 +0000

    Fix some errors, looking better

--- gpsout.c
+++ gpsout.c
@@ -16,6 +16,15 @@
 #include <mosquitto.h>
 #include <math.h>
 
+typedef struct point_s point_t;
+struct point_s
+{
+   int id;
+   time_t utc;
+   double lat,
+     lon;
+};
+
 int
 main (int argc, const char *argv[])
 {
@@ -29,12 +38,10 @@ main (int argc, const char *argv[])
    const char *to = NULL;
    const char *device = NULL;
    int debug = 0;
-   int maxtime = 60;
-   double maxdist = 100;
-   double maxerror = 0.1;
-   int prune = 0;
+   double margin = 0.1;
    int json = 0;
    int quiet = 0;
+   int delete = 0;
    {                            // POPT
       poptContext optCon;       // context for parsing command-line options
       const struct poptOption optionsTable[] = {
@@ -45,15 +52,13 @@ main (int argc, const char *argv[])
          {"sql-password", 'P', POPT_ARG_STRING, &sqlpassword, 0, "SQL password", "pass"},
          {"sql-table", 't', POPT_ARG_STRING | POPT_ARGFLAG_SHOW_DEFAULT, &sqltable, 0, "SQL table", "table"},
          {"sql-debug", 'v', POPT_ARG_NONE, &sqldebug, 0, "SQL Debug"},
-         {"max-time", 0, POPT_ARG_INT | POPT_ARGFLAG_SHOW_DEFAULT, &maxtime, 0, "Max time pruned", "seconds"},
-         {"max-dist", 0, POPT_ARG_DOUBLE | POPT_ARGFLAG_SHOW_DEFAULT, &maxdist, 0, "Max dist pruned", "m"},
-         {"max-error", 0, POPT_ARG_DOUBLE | POPT_ARGFLAG_SHOW_DEFAULT, &maxerror, 0, "Max error pruned", "m"},
+         {"margin", 0, POPT_ARG_DOUBLE | POPT_ARGFLAG_SHOW_DEFAULT, &margin, 0, "Max allow error", "m"},
          {"device", 'D', POPT_ARG_STRING, &device, 0, "Device", "ID"},
          {"from", 'F', POPT_ARG_STRING, &from, 0, "From", "YYYY-MM-DD HH:MM:SS (UTC)"},
          {"to", 'T', POPT_ARG_STRING, &to, 0, "To", "YYYY-MM-DD HH:MM:SS (UTC)"},
-         {"prune", 0, POPT_ARG_NONE, &prune, 0, "Do pruning"},
          {"json", 0, POPT_ARG_NONE, &json, 0, "JSON outout"},
          {"quiet", 0, POPT_ARG_NONE, &quiet, 0, "Quiet"},
+         {"delete", 0, POPT_ARG_NONE, &delete, 0, "Delete pruned from database"},
          {"debug", 'V', POPT_ARG_NONE, &debug, 0, "Debug"},
          POPT_AUTOHELP {}
       };
@@ -80,135 +85,100 @@ main (int argc, const char *argv[])
       tto = sql_time_utc (to);
    SQL sql;
    sql_real_connect (&sql, sqlhostname, sqlusername, sqlpassword, sqldatabase, 0, NULL, 0, 1, sqlconffile);
+   // Get points
    SQL_RES *res = sql_safe_query_store_free (&sql,
                                              sql_printf ("SELECT * FROM `%#S` WHERE `utc`>=%#T AND `utc`<=%#T AND `device`=%#s",
                                                          sqltable,
                                                          tfrom, tto, device));
+   int num = sql_num_rows (res);
+   point_t *points = malloc (num * sizeof (*points));
+   memset (points, 0, num * sizeof (*points));
+   int n = 0;
+   while (sql_fetch_row (res) && n < num)
+   {
+      points[n].id = atoi (sql_colz (res, "ID"));
+      points[n].utc = sql_time_utc (sql_colz (res, "utc"));
+      points[n].lat = strtod (sql_colz (res, "lat"), NULL);
+      points[n].lon = strtod (sql_colz (res, "lon"), NULL);
+      n++;
+   }
+   sql_free_result (res);
+   void prune (int l, int h)
+   {
+      if (l + 1 >= h)
+         return;                // Not intermediate points
+      point_t *a = &points[l];
+      point_t *b = &points[h];
+      // Centre for working out metres
+      double clat = (a->lat + b->lat) / 2;
+      double clon = (a->lon + b->lon) / 2;
+      double slat = 111111.0 * cos (M_PI * clat / 180.0);
+      inline double x (point_t * p)
+      {
+         return (p->lon - clon) * slat;
+      }
+      inline double y (point_t * p)
+      {
+         return (p->lat - clat) * 111111.0;
+      }
+      double B = sqrt ((y (b) - y (a)) * (y (b) - y (a)) + (x (b) - x (a)) * (x (b) - x (a)));
+      int bestn = -1;
+      double best = 0;
+      int n;
+      for (n = l + 1; n < h; n++)
+      {
+         point_t *p = &points[n];
+         double d = 0;
+         if (!B)
+            d = sqrt ((x (p) - x (a)) * (x (p) - x (a)) + (y (p) - y (a)) * (y (p) - y (a)));
+         else
+         {
+            double A = ((y (b) - y (a)) * x (p) - (x (b) - x (a)) * y (p) + x (b) * y (a) - y (b) * x (a));
+            if (A < 0)
+               A = 0.0 - A;
+            d = A / B;
+         }
+         if (bestn >= 0 && d < best)
+            continue;
+         bestn = n;             // New furthest
+         best = d;
+      }
+      if (debug)
+         fprintf (stderr, "Prune %d/%d/%d %lfm\n", l, bestn, h, best);
+      if (best < margin)
+      {                         // All points are within margin - so all to be pruned
+         for (n = l + 1; n < h; n++)
+         {
+            if (delete)
+               sql_safe_query_free (&sql, sql_printf ("DELETE FROM `%#S` WHERE `ID`=%d", sqltable, points[n].id));
+            points[n].id = 0;
+         }
+         return;
+      }
+      prune (l, bestn);
+      prune (bestn, h);
+   }
+   if (margin)
+      prune (0, num - 1);
+   // Output
    if (json)
       printf ("[\n");
-   double scalelat,
-     scalelon;
-   double maxsquare = maxdist * maxdist;
-   struct point_s
-   {
-      struct point_s *next;
-      int id;
-      time_t utc;
-      double lat,
-        lon;
-      double x,
-        y;                      // relative to first point
-      char prune;
-   } *first = NULL, *last = NULL;
-   while (sql_fetch_row (res))
-   {
-      void output (time_t utc, double lat, double lon)
+   for (n = 0; n < num; n++)
+      if (points[n].id)
       {
          if (json)
          {
             struct tm t;
-            localtime_r (&utc, &t);
+            localtime_r (&points[n].utc, &t);
             char temp[22];
             strftime (temp, sizeof (temp), "%F %T", &t);
-            printf ("{\"utc\":\"%s\",\"lat\":%lf,\"lon\":%lf}\n", temp, lat, lon);
+            printf ("{\"utc\":\"%s\",\"lat\":%lf,\"lon\":%lf}\n", temp, points[n].lat, points[n].lon);
          } else if (!quiet)
-            printf ("%lf,%lf\n", lat, lon);
+            printf ("%lf,%lf\n", points[n].lat, points[n].lon);
       }
-      double lat = strtod (sql_colz (res, "lat"), NULL);
-      double lon = strtod (sql_colz (res, "lon"), NULL);
-      time_t utc = sql_time (sql_colz (res, "utc"));
-      if (!prune)
-      {
-         output (utc, lat, lon);
-         continue;
-      }
-      struct point_s *p = malloc (sizeof (*p));
-      memset (p, 0, sizeof (*p));
-      p->id = atoi (sql_colz (res, "ID"));
-      p->utc = utc;
-      p->lat = lat;
-      p->lon = lon;
-      void newfirst (void)
-      {
-         last = first;
-         first->x = 0;
-         first->y = 0;
-         scalelat = (double) 11111;     // Yes, constant for that is based on French definition of metre
-         scalelon = (double) 11111 *cos (first->lat);
-      }
-      if (first && (p->utc - first->utc > maxtime || (p->x * p->x) * (p->y * p->y) > maxsquare))
-      {
-         double dist (struct point_s *a, struct point_s *p, struct point_s *b)
-         {                      // Distance of point p from line a/b
-            if (a->x == b->x && a->y == b->y)
-               return sqrt ((p->x - a->x) * (p->x - a->x) + (p->y - a->y) * (p->y - a->y));     // Point
-            double A = (b->y - a->y) * p->x - (b->x - a->x) * p->y + b->x * a->y - b->y * a->x;
-            if (A < 0)
-               A = 0.0 - A;
-            double B = sqrt ((b->y - a->y) * (b->y - a->y) + (b->x - a->x) * (b->x - a->x));
-            return A / B;
-
-         }
-         int rdp (struct point_s *f, struct point_s *t)
-         {                      // Prune: https://en.wikipedia.org/wiki/Ramer–Douglas–Peucker_algorithm
-            f->prune = 0;
-            t->prune = 0;
-            if (f == t || f->next == t)
-               return 0;        // Nothing to prune
-            struct point_s *p,
-             *b = NULL;
-            double best = 0;
-            for (p = f->next; p != t; p = p->next)
-            {
-               double d = dist (f, p, t);
-               if (!b || d > best)
-               {
-                  b = p;
-                  best = d;
-               }
-            }
-            if (best < maxerror)
-            {                   // Prune all
-               int n = 0;
-               for (p = f->next; p != t; p = p->next)
-               {
-                  p->prune = 1;
-                  n++;
-               }
-               return n;
-            }
-            // Recurse
-            return rdp (f, b) + rdp (b, t);
-         }
-         rdp (first, last);
-         while (first->next)
-         {
-            if (!first->prune)
-               output (first->utc, first->lat, first->lon);
-            struct point_s *n = first->next;
-            free (first);
-            first = n;
-         }
-         last = first;
-         newfirst ();
-      }
-      if (first)
-      {                         // Offset data
-         p->y = (p->lat - first->lat) * scalelat;
-         p->x = (p->lon - first->lon) * scalelon;
-      }
-      if (first)
-         last->next = p;
-      else
-      {
-         first = p;
-         newfirst ();
-      }
-      last = p;
-   }
    if (json)
       printf ("]\n");
-   sql_free_result (res);
+   free (points);
    sql_close (&sql);
    return 0;
 }
